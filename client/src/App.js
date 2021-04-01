f804b68df9563e0c20d05847774255e98095e33c
ankushanubhav <ankushanubhav@gmail.com>
2021-04-01 17:00:48 +0530

    Deploying to AWS

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="Fib Calculator"
             target="_blank"
             rel="noopener noreferrer"
           >
